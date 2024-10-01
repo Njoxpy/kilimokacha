1967e6c43dc328c0643405287e535ebb4ebc4008
Njox <godblessnyagawa@gmail.com>
2024-10-02 00:06:39 +0300

    idid some testing into the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,106 +1,103 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 
-
-// pages
+// Pages
 import AddBlog from './blog/auth/AddBlog';
-import Fetch from './hooks/Fetch';
 import Home from "./Home/Home";
 import HomeBlog from "./blog/Home/HomeBlog";
-import NotFound from "./pages/NotFound"
+import NotFound from "./Error/NotFound";
 import SearchBlogCategory from "./blog/components/SearchBlogCategory";
-import FarmingTips from "./blog/pages/FarmingTips"
-import MarketTrends from "./blog/pages/MarketTrends"
-import SuccessStories from "./blog/pages/SuccessStories"
-import Sustainability from "./blog/pages/Sustainability"
-import MarketInfo from "./pages/MarketInfo"
-import Weather from "./pages/Weather"
-import Login from "./auth/Login"
-import Register from "./auth/Register"
-import Announcement from "./pages/Announcement"
-import BlogPost from "./blog/pages/BlogPost"
-import AddExpertAdvice from "./pages/AddExpertAdvice";
-import UserProfile from "./pages/UserProfile";
-import ExpertAdvice from "./pages/ExpertAdvice"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Terms from "./pages/Terms"
-import Privacy from "./pages/Privacy"
-import BlogAuthor from "./blog/pages/BlogAuthor"
+import FarmingTips from "./blog/pages/FarmingTips";
+import MarketTrends from "./blog/pages/MarketTrends";
+import SuccessStories from "./blog/pages/SuccessStories";
+import Sustainability from "./blog/pages/Sustainability";
+import MarketInfo from "./pages/market/MarketInfo";
+import Weather from "./pages/weather/Weather";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Announcement from "./pages/announcement/Announcement";
+import BlogPost from "./blog/pages/BlogPost";
+import AddExpertAdvice from "./pages/expert_advice/AddExpertAdvice";
+import UserProfile from "./pages/user_profile/UserProfile";
+import ExpertAdvice from "./pages/expert_advice/ExpertAdvice";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Terms from "./pages/terms/Terms";
+import Privacy from "./pages/terms/Privacy";
+import BlogAuthor from "./blog/pages/BlogAuthor";
 
-// layouts: root
+// Layouts
 import RootLayout from "./layouts/RootLayout";
 import ExpertAdviceLayout from "./layouts/ExpertAdviceLayout";
 import WeatherLayout from "./layouts/WeatherLayout";
 import UserProfileLayout from "./layouts/UserProfileLayout";
-import AnnouncementLayout from "./layouts/AnnouncementLayout"
-
-// layouts: blog
+import AnnouncementLayout from "./layouts/AnnouncementLayout";
 import BlogAuthorLayout from "./blog/Layouts/BlogAuthorLayout";
 import CategoryLayout from "./blog/Layouts/CategoryLayout";
 
-// hooks
-
-// details
-import ExpertAdviceDetails from "./pages/ExpertAdviceDetails";
-import ForgotPassword from "./auth/ForgotPassword"
-import UserProfileDetails from "./pages/UserProfileDetails"
-import WeatherDetails from "./pages/WeatherDetails";
+// Hooks
+import ExpertAdviceDetails from "./pages/expert_advice/ExpertAdviceDetails";
+import ForgotPassword from "./auth/ForgotPassword";
+import UserProfileDetails from "./pages/user_profile/UserProfileDetails";
+import WeatherDetails from "./pages/weather/WeatherDetails";
 import BlogAuthorDetails from "./blog/pages/BlogAuthorDetails";
-import AnnouncementDetails from "./pages/AnnouncementDetails"
+import AnnouncementDetails from "./pages/announcement/AnnouncementDetails";
 
-// loader
-import { blogsLoader } from "./blog/pages/Blog";
-
-// error
+// Error Components
 import ExpertAdviceError from "./Error/ExpertAdviceError";
-import WeatherError from "./Error/WeatherError"
+import WeatherError from "./Error/WeatherError";
 import UserError from "./Error/UserError";
 import AnnouncementError from "./Error/AnnouncementError";
 
+// Router
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="/fetch" element={<Fetch />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="market-info" element={<MarketInfo />} />
-      <Route path="announcement" element={<AnnouncementLayout />}>
+      <Route path={"market-info"} element={<MarketInfo />} />
+
+      <Route path={"announcement"} element={<AnnouncementLayout />}>
         <Route index element={<Announcement />} />
-        <Route path=":announceId" element={<AnnouncementDetails />} errorElement={<AnnouncementError />}/>
+        <Route path={":announceId"} element={<AnnouncementDetails />} errorElement={<AnnouncementError />} />
       </Route>
-      <Route path="weather-data" element={<WeatherLayout />} >
+
+      <Route path={"weather-data"} element={<WeatherLayout />}>
         <Route index element={<Weather />} />
-        <Route path=":location" element={<WeatherDetails />} errorElement={<WeatherError />} />
+        <Route path={":location"} element={<WeatherDetails />} errorElement={<WeatherError />} />
       </Route>
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
+
+      <Route path={"about"} element={<About />} />
+      <Route path={"contact"} element={<Contact />} />
       <Route path="terms" element={<Terms />} />
       <Route path="privacy" element={<Privacy />} />
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Register />} />
+      <Route path="reset-password" element={<ForgotPassword />} />
+
       <Route path="user" element={<UserProfileLayout />}>
-        <Route path="user" element={<UserProfile />} />
+        <Route index element={<UserProfile />} />
         <Route path=":username" element={<UserProfileDetails />} errorElement={<UserError />} />
       </Route>
-      <Route path="reset-password" element={<ForgotPassword />} />
-      <Route path="expert-advice" element={<ExpertAdviceLayout />} >
+
+      <Route path="expert-advice" element={<ExpertAdviceLayout />}>
         <Route index element={<ExpertAdvice />} />
         <Route path=":id" element={<ExpertAdviceDetails />} errorElement={<ExpertAdviceError />} />
+        <Route path="new" element={<AddExpertAdvice />} />
       </Route>
-      <Route path="add-expert-advice" element={<AddExpertAdvice />} />
-      <Route path="/blog" element={<CategoryLayout />} errorElement={<NotFound />}>
+
+      <Route path="*" element={<NotFound />} />
+      <Route path="blog" element={<CategoryLayout />} errorElement={<NotFound />}>
         <Route index element={<HomeBlog />} />
-        <Route path="blog/:id" element={<BlogPost />} sear/>
-        <Route path="author" element={<BlogAuthorLayout />}>
-          <Route index element={<BlogAuthor />} />
-          <Route path=":name" element={<BlogAuthorDetails />} />
-        </Route>
-        <Route path="add" element={<AddBlog />} />
+        <Route path="new" element={<AddBlog />} />
         <Route path="search" element={<SearchBlogCategory />} />
         <Route path="farming-tips" element={<FarmingTips />} />
         <Route path="market-trends" element={<MarketTrends />} />
         <Route path="success-stories" element={<SuccessStories />} />
         <Route path="sustainability" element={<Sustainability />} />
+        <Route path=":id" element={<BlogPost />} />
+        <Route path="authors" element={<BlogAuthorLayout />}>
+          <Route index element={<BlogAuthor />} />
+          <Route path=":name" element={<BlogAuthorDetails />} />
+        </Route>
       </Route>
     </Route>
   )

c024bcc71dc2c6e049cc70c555d598914f7d527d
Njox <godblessnyagawa@gmail.com>
2024-10-02 14:03:47 +0300

    updated accorsing to gpt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Make sure this is set to 'dist'
-  }
+    outDir: 'dist', // This sets the output directory for the build
+  },
 })

169c45a32fbb9568232e4c3a9c18e9c7062e6455
Njox <godblessnyagawa@gmail.com>
2024-10-06 19:52:26 +0300

    added padding

--- frontend/src/pages/weather/WeatherDetails.jsx
+++ frontend/src/pages/weather/WeatherDetails.jsx
@@ -6,7 +6,7 @@ const WeatherDetails = () => {
     const {location} = useParams();
     return(
         <>
-        <div>
+        <div className="p-4">
             <h4>{location}</h4>
         </div>
         <Footer />

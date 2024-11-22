7aaed3f0312f9537408d4a48a70767a7e3bc835b
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-22 10:32:22 +0300

    added loading jsx components and useError fetch incase there is an error when fetching

---
+++ frontend/src/components/ErrorFetch.jsx
@@ -0,0 +1,11 @@
+const ErrorFetch = () => {
+    return (
+        <>
+            <div>
+                <div className="text-red-500 text-center mt-4 font-bold">error when fetching data.</div>
+            </div>
+        </>
+    )
+}
+
+export default ErrorFetch;
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <>
+            <div>
+                <div className="text-center text-yellow-700 mt-4 font-bold">Loading...</div>
+            </div>
+        </>
+    )
+}
+
+export default Loading;

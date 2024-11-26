abe6ab58a7b23527ea4636b824c9440f9a0a42dc
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-26 20:53:37 +0300

    added good styling for blogList

--- frontend/src/blog/pages/BlogList.jsx
+++ frontend/src/blog/pages/BlogList.jsx
@@ -1,53 +1,129 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import brocoili from "../../assets/images/brocoli.jpg"
+import ceo from "../../assets/images/godbless.jpeg"
+import { Link } from "react-router-dom"
+
+const posts = [
+    {
+        id: 1,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        imageUrl:
+            brocoili,
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '/blog/market-trends' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl: ceo,
+        },
+    },
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        imageUrl:
+            brocoili,
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '/blog/market-trends' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl: ceo,
+        },
+    },
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        imageUrl:
+            brocoili,
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '/blog/market-trends' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl: ceo,
+        },
+    }
+]
 
 const BlogList = () => {
-    const URL = "http://localhost:3002/blogs";
-    const [blogs, setBlogs] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        fetch(URL)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then((blogs) => {
-                setBlogs(blogs);
-                setLoading(false);
-            })
-            .catch((error) => {
-                setError(error.message);
-                setLoading(false);
-            });
-    }, []);
-
-    if (loading) return <div className="text-center">Loading...</div>;
-    if (error) return <div className="text-red-500 text-center">{error}</div>;
-
     return (
-        <div className="p-4">
-            {blogs.map((blog) => (
-                <Link key={blog.id} to={`/blog/${blog.id}`} className="block border-b border-green-100 p-4 hover:bg-green-50 transition-colors duration-300">
-                    <div>
-                        <img src={blog.thumbnail_image} alt={blog.blog_title} className="w-full h-auto mb-2 rounded-lg" />
-                        <h2 className="text-2xl font-bold">{blog.blog_title}</h2>
-                        <small className="text-gray-700 mt-2 block">{blog.author_name}</small>
-                        <p className="text-gray-700 mb-4">{blog.snippets}</p>
+        <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:max-w-4xl">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                    <p className="mt-2 text-lg leading-8 text-gray-600">
+                        Learn how to grow your business with our expert advice.
+                    </p>
+                    <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+                        {posts.map((post) => (
+                            <Link to={`blog/${post.id}`}>
+                                <article key={post.id} className="relative isolate flex flex-col gap-8 lg:flex-row mt-4">
+                                    <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                                        <img
+                                            alt=""
+                                            src={post.imageUrl}
+                                            className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                                        />
+                                        <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                                    </div>
+                                    <div>
+                                        <div className="flex items-center gap-x-4 text-xs">
+                                            <time dateTime={post.datetime} className="text-gray-500">
+                                                {post.date}
+                                            </time>
+                                            <a
+                                                href={post.category.href}
+                                                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                                            >
+                                                {post.category.title}
+                                            </a>
+                                        </div>
+                                        <div className="group relative max-w-xl">
+                                            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                                <a href={post.href}>
+                                                    <span className="absolute inset-0" />
+                                                    {post.title}
+                                                </a>
+                                            </h3>
+                                            <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
+                                        </div>
+                                        <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+                                            <div className="relative flex items-center gap-x-4">
+                                                <img alt="" src={post.author.imageUrl} className="h-10 w-10 rounded-full bg-gray-50" />
+                                                <div className="text-sm leading-6">
+                                                    <p className="font-semibold text-gray-900">
+                                                        <a href={post.author.href}>
+                                                            <span className="absolute inset-0" />
+                                                            {post.author.name}
+                                                        </a>
+                                                    </p>
+                                                    <p className="text-gray-600">{post.author.role}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            </Link>
+                        ))}
                     </div>
-                </Link>
-            ))}
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default BlogList;
-
-const blogsLoader = async () => {
-    const res = await fetch("http://localhost:3002/blogs")
-
-    return res.json()
-}

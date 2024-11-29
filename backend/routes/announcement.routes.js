d96600b470560ce0bdd0b996ce68557e42c473b1
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-29 10:40:28 +0300

    added basic routes for announcement

--- backend/routes/announcement.routes.js
+++ backend/routes/announcement.routes.js
@@ -1,8 +1,16 @@
 const express = require("express")
-const { getAllAnnouncements, createAnnouncement } = require("../controllers/announcement.controller")
+const { getAllAnnouncements, createAnnouncement, updateAnnouncementBydId, getAnnouncementById, deleteAnnouncementById } = require("../controllers/announcement.controller")
 const router = express.Router()
 
+// get all announcement
 router.get("/", getAllAnnouncements)
+// get announcement by id
+router.get("/:id", getAnnouncementById)
+// create announcement
 router.post("/", createAnnouncement)
+// update announcement
+router.patch("/", updateAnnouncementBydId)
+// delete announcement by id
+router.delete("/:id", deleteAnnouncementById)
 
 module.exports = router

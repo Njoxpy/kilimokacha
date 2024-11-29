5a0916aa75fdeb91e157d123db34c426888d279a
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-29 10:39:36 +0300

    added get blog by id, updated blog by id and delete by id

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -15,6 +15,21 @@ const getAllBlogs = async (req, res) => {
 }
 
 // get blog by id
+const getBlogBydId = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.json({ message: "blog not found" })
+    }
+    try {
+        const blog = await Blog.findById({ _id: id })
+        if (!blog) {
+            return res.json({ mesage: 'could not get blog with that details' })
+        }
+        res.status(200).json(blog)
+    } catch (error) {
+        res.status(404).json({ message: "could not fetch blog", details: error })
+    }
+}
 
 // get blogs by category
 
@@ -33,7 +48,48 @@ const createBlog = async (req, res) => {
     }
 }
 
+// update blog by id
+const updateBlogById = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.json({ message: "blog not found" })
+    }
+    try {
+        const updatedBlog = await Blog.findOneAndUpdate({ _id: id }, {
+            ...req.body
+        }, { new: true })
+        if (!updatedBlog) {
+            return res.status(404).json({ message: "could not get the blog with that id" })
+        }
+        res.status(200).json({ message: "updated sucessfully", details: updatedBlog })
+    } catch (error) {
+        res.status(404).json({ message: "could not update blog", details: error })
+    }
+}
+
+// delete blog by id
+const deleteBlogById = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.json({ message: "blog not found" })
+    }
+    try {
+        const deletedBlog = await Blog.findOneAndDelete({ _id: id })
+        if (!deletedBlog) {
+            return res.status(404).json({ message: "could not get the blog" })
+        }
+        res.status(200).json({ message: `deleted sucessfully: ${deletedBlog}` })
+    } catch (error) {
+        res.status(404).json({ message: "could not update blog", details: error })
+    }
+}
+
+
+
 module.exports = {
     getAllBlogs,
-    createBlog
+    createBlog,
+    getBlogBydId,
+    updateBlogById,
+    deleteBlogById
 }

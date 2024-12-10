00d9a971b638df2b222103012971e4d5c5c74bcc
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-12-10 18:41:43 +0300

    added validate object id middleware

---
+++ backend/middleware/validateObjectId.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const {NOT_FOUND} = require("../constants/responseCode")
+
+const validateObjectId = (req, res, next) => {
+    const {id} = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(NOT_FOUND).json({message:"Item not found"})
+    }
+    next()
+}
+
+module.exports = validateObjectId

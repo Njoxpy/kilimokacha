329f01b8aa9cc11f89812ef5b56068b975e9f5a4
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-12-16 19:29:14 +0300

    added providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+// context
 import { MarketsContextProvider } from './context/MarketContext.jsx'
+import { AnnouncementContextProvider } from './context/AnnouncementContext.jsx'
+import { BlogContextProvider } from './context/BlogContext.jsx'
+import { ExpertAdviceContextProvider } from './context/ExpertAdviceContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MarketsContextProvider>
-    <App />
+      <AnnouncementContextProvider>
+        <BlogContextProvider>
+          <ExpertAdviceContextProvider>
+            <App />
+          </ExpertAdviceContextProvider>
+        </BlogContextProvider>
+      </AnnouncementContextProvider>
     </MarketsContextProvider>
   </StrictMode>,
 )

53571762bc913e850c3700176de9f0362e87c8db
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-12-16 19:27:43 +0300

    removed unused code and refactored

--- frontend/src/Error/NotFound.jsx
+++ frontend/src/Error/NotFound.jsx
@@ -1,16 +1,12 @@
-import { Link, useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    const error = useRouteError();
 
     return (
         <>
             <div className="p-4">
                 <h1>Ooops!</h1>
                 <p>Sorry, an expected error has occured.</p>
-                <p>
-                    <i>{error.statusText || error.message}</i>
-                </p>
                 <h3>Page not found</h3>
                 <p>Sorry the page you were looking for was not found, return to <Link to="/">homepage</Link></p>
             </div>
